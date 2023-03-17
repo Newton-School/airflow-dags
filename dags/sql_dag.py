34ae35c7e9d4ed3dbb093f5e602f2a246ee1fea5
harshalmahajan <harshal.mahajan@newtonschool.co>
2023-03-17 14:31:14 +0530

    Fix SQL Dag

--- dags/sql_dag.py
+++ dags/sql_dag.py
@@ -35,7 +35,7 @@ create_table = PostgresOperator(
 transform_data = PostgresOperator(
     task_id='transform_data',
     postgres_conn_id='postgres_read_replica',
-    sql='''SELECT * FROM courses_course;
+    sql='''SELECT id,title FROM courses_course;
         ''',
     dag=dag
 )
