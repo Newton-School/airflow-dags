e9e346276b9118888a5f04bc5b633952d1b273e3
Drumil Patel <drumilpatel720@gmail.com>
2023-03-17 18:42:33 +0530

    Fix Sql Dag
    Signed-off-by: Drumil Patel <drumilpatel720@gmail.com>

--- dags/sql_dag.py
+++ dags/sql_dag.py
@@ -37,7 +37,7 @@ create_table = PostgresOperator(
             username varchar(100),
             email varchar(100),
             name varchar(100),
-            phone varchar(15),
+            phone varchar(15)
         );
     ''',
     dag=dag
