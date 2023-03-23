e41502587c89ffd25f7f32f6821d8f2c4464a0e7
harshalmahajan <harshal.mahajan@newtonschool.co>
2023-03-23 14:20:15 +0530

    Fix Dags
    Signed-off-by: harshalmahajan <harshal.mahajan@newtonschool.co>

--- dags/sql_dag.py
+++ dags/sql_dag.py
@@ -16,7 +16,7 @@ def extract_data_to_nested(**kwargs):
     ti = kwargs['ti']
     transform_data_output = ti.xcom_pull(task_ids='transform_data')
     for transform_row in transform_data_output:
-        insert_query = f'INSERT INTO user_details_test_3 (user_id,username,email,name,phone) VALUES ({transform_row[0]},\'{transform_row[1]}\',\'{transform_row[2]}\',\'{transform_row[3]}"\', {transform_row[4]});'
+        insert_query = f'INSERT INTO user_details_test_3 (user_id,username,email,name,phone,last_login) VALUES ({transform_row[0]},\'{transform_row[1]}\',\'{transform_row[2]}\',\'{transform_row[3]}\', \'{transform_row[4]}\',CAST(\'{transform_row[5]}\' As TIMESTAMP));'
         print(insert_query)
         pg_hook.run(insert_query)
 
@@ -31,13 +31,14 @@ dag = DAG(
 create_table = PostgresOperator(
     task_id='create_table',
     postgres_conn_id='postgres_result_db',
-    sql='''CREATE TABLE IF NOT EXISTS user_details_test_3 (
+    sql='''CREATE TABLE IF NOT EXISTS user_details_test_4 (
             id SERIAL PRIMARY KEY,
             user_id bigint not null,
             username varchar(100),
             email varchar(100),
             name varchar(100),
-            phone varchar(15)
+            phone varchar(15),
+            last_login timestamp
         );
     ''',
     dag=dag
@@ -46,19 +47,19 @@ create_table = PostgresOperator(
 transform_data = PostgresOperator(
     task_id='transform_data',
     postgres_conn_id='postgres_read_replica',
-    sql='''SELECT CAST(last_login as VARCHAR) asm,auth_user.id,username,email,concat(first_name,' ',last_name) as name,users_userprofile.phone
+    sql='''SELECT auth_user.id,username,email,concat(first_name,' ',last_name) as name,users_userprofile.phone,CAST(last_login as VARCHAR) as last_login
             FROM auth_user
             left join users_userprofile on users_userprofile.user_id = auth_user.id;
         ''',
     dag=dag
 )
 
-# extract_python_data = PythonOperator(
-#     task_id='extract_python_data',
-#     python_callable=extract_data_to_nested,
-#     provide_context=True,
-#     dag=dag
-# )
+extract_python_data = PythonOperator(
+    task_id='extract_python_data',
+    python_callable=extract_data_to_nested,
+    provide_context=True,
+    dag=dag
+)
 
 # extract_data = PostgresOperator(
 #     task_id='extract_data',
