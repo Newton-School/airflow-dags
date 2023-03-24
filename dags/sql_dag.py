64cad8633349f2983890e896147c62837e5f4098
Drumil Patel <drumilpatel720@gmail.com>
2023-03-24 18:17:32 +0530

    Fix
    Signed-off-by: Drumil Patel <drumilpatel720@gmail.com>

--- dags/sql_dag.py
+++ dags/sql_dag.py
@@ -49,6 +49,7 @@ def extract_data_to_nested(**kwargs):
         #                f'{clean_input("datetime",transform_row[5])}' \
         #                f');'
         # pg_hook.run(insert_query)
+    pg_conn.commit()
 
 
 dag = DAG(
