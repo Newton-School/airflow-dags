7248d29b330082508893728e8e2bd28409a64de0
Drumil Patel <drumilpatel720@gmail.com>
2023-04-03 23:40:04 +0530

    Fix Dags
    Signed-off-by: Drumil Patel <drumilpatel720@gmail.com>

--- dags/xcom/S3CustomBackend.py
+++ dags/xcom/S3CustomBackend.py
@@ -9,7 +9,7 @@ from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 
 
 class S3CustomBackendXCom(BaseXCom):
-	S3_BUCKET_NAME = "airflow_dags"
+	S3_BUCKET_NAME = "newton-airflow-dags-temp"
 	S3_PREFIX = "dags-temp/"
 
 	@staticmethod
