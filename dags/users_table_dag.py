bad291c3b184b171afd360201cd419466e6b7c09
harshalmahajan <harshal.mahajan@newtonschool.co>
2023-04-12 18:06:15 +0530

    Fix DAG
    Signed-off-by: harshalmahajan <harshal.mahajan@newtonschool.co>

--- dags/users_table_dag.py
+++ dags/users_table_dag.py
@@ -153,8 +153,7 @@ select distinct auth_user.id as user_id,auth_user.first_name,auth_user.last_name
         date_of_birth,utm_source,utm_medium,utm_campaign,
         tenth_marks,twelfth_marks,bachelors_marks,bachelors_grad_year,bachelors_degree,bachelors_field_of_study,masters_marks,masters_grad_year,masters_degree,masters_field_of_study
     from t1
-        where rank =1
-        limit 500000;
+        where rank =1;
         ''',
     dag=dag
 )
