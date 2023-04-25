51cff4550d1f6451e4e21a4d40b4c59d2169d854
Drumil Patel <drumilpatel720@gmail.com>
2023-04-25 19:44:32 +0530

    Fix Assignment Question User Mapping
    Signed-off-by: Drumil Patel <drumilpatel720@gmail.com>

--- dags/assignment_question_user_mapping_dag.py
+++ dags/assignment_question_user_mapping_dag.py
@@ -394,8 +394,8 @@ def transform_data_per_query(start_assignment_id, end_assignment_id, cps_sub_dag
                         and questions_released.question_id is not null
                         group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,assignments_assignmentcourseuserquestionmapping.coding_playground_id,assignments_assignmentcourseuserquestionmapping.front_end_playground_id,assignments_assignmentcourseuserquestionmapping.game_playground_id,assignments_assignmentcourseuserquestionmapping.project_playground_id
         ) final_query
-        limit {{ ti.xcom_pull(key=format('transforming_data_{}.extract_and_transform_individual_assignment_sub_dag_{}_cps_sub_dag_{}.limit_offset_generator.limit', params.current_assignment_sub_dag_id, params.current_assignment_sub_dag_id, params.current_cps_sub_dag_id )) }}, 
-        {{ ti.xcom_pull(key=format('transforming_data_{}.extract_and_transform_individual_assignment_sub_dag_{}_cps_sub_dag_{}.limit_offset_generator.limit', params.current_assignment_sub_dag_id, params.current_assignment_sub_dag_id, params.current_cps_sub_dag_id )) }}
+        limit {{ ti.xcom_pull(key='transforming_data_{}.extract_and_transform_individual_assignment_sub_dag_{}_cps_sub_dag_{}.limit_offset_generator.limit'.format(params.current_assignment_sub_dag_id, params.current_assignment_sub_dag_id, params.current_cps_sub_dag_id )) }}, 
+        {{ ti.xcom_pull(key='transforming_data_{}.extract_and_transform_individual_assignment_sub_dag_{}_cps_sub_dag_{}.limit_offset_generator.offset'.format(params.current_assignment_sub_dag_id, params.current_assignment_sub_dag_id, params.current_cps_sub_dag_id )) }}
         ;
             ''' % (start_assignment_id, end_assignment_id, start_assignment_id, end_assignment_id, start_assignment_id, end_assignment_id),
     )
