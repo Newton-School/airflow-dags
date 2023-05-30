4b805c1eca936377a5e5f633aa8f3afcfd841f3d
aniket_vijay <aniket.vijay@newtonschool.co>
2023-05-30 18:21:18 +0530

    pushing assessment topic mapping dag
    Signed-off-by: aniket_vijay <aniket.vijay@newtonschool.co>

--- dags/assessment_topic_mapping_dag.py
+++ dags/assessment_topic_mapping_dag.py
@@ -52,7 +52,7 @@ dag = DAG(
     'assessment_topic_mapping_dag',
     default_args=default_args,
     description='Assessment mapping with topics',
-    schedule_interval='30 16 * * *',
+    schedule_interval='0 2 * * *',
     catchup=False
 )
 
