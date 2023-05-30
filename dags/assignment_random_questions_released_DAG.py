8c6fab6ecefdbe08f3cc62f6fd07cd59e8888fe5
aniket_vijay <aniket.vijay@newtonschool.co>
2023-05-30 18:30:51 +0530

    pushing assign question random flow dag
    Signed-off-by: aniket_vijay <aniket.vijay@newtonschool.co>

--- dags/assignment_random_questions_released_DAG.py
+++ dags/assignment_random_questions_released_DAG.py
@@ -46,7 +46,7 @@ dag = DAG(
     'assignment_random_questions_released_DAG',
     default_args=default_args,
     description='A DAG for assignment questions released though the random flow',
-    schedule_interval='30 16 * * *',
+    schedule_interval='30 20 * * *',
     catchup=False
 )
 
