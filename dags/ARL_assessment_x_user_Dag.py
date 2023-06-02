3f16a7270ac7f6cedd70f8f86f9b38d996440970
harshalmahajan <harshal.mahajan@newtonschool.co>
2023-06-02 18:47:05 +0530

    Add assessment x user ARL dag
    Signed-off-by: harshalmahajan <harshal.mahajan@newtonschool.co>

---
+++ dags/ARL_assessment_x_user_Dag.py
@@ -0,0 +1,170 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from datetime import datetime
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 3, 16),
+}
+
+
+def extract_data_to_nested(**kwargs):
+    def clean_input(data_type, data_value):
+        if data_type == 'string':
+            return 'null' if not data_value else f'\"{data_value}\"'
+        elif data_type == 'datetime':
+            return 'null' if not data_value else f'CAST(\'{data_value}\' As TIMESTAMP)'
+        else:
+            return data_value
+
+    pg_hook = PostgresHook(postgres_conn_id='postgres_result_db')
+    pg_conn = pg_hook.get_conn()
+    pg_cursor = pg_conn.cursor()
+    ti = kwargs['ti']
+    transform_data_output = ti.xcom_pull(task_ids='transform_data')
+    for transform_row in transform_data_output:
+        pg_cursor.execute(
+            'INSERT INTO arl_assessments_x_user (table_unique_key,user_id,course_id,assessment_id,'
+            'assessment_title,assessment_type,assessment_sub_type,assessment_release_date,'
+            'assessment_open_date,assessment_submission_date,assessment_attempt_status,'
+            'assessment_submission_status,question_count,questions_marked,questions_correct,'
+            'max_marks,marks_obtained,cheated)'
+            'VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+            'on conflict (table_unique_key) do update set assessment_open_date=Excluded.assessment_open_date,'
+            'assessment_submission_date=EXCLUDED.assessment_submission_date,'
+            'assessment_attempt_status=EXCLUDED.assessment_attempt_status,'
+            'assessment_submission_status=EXCLUDED.assessment_submission_status,'
+            'question_count=EXCLUDED.question_count,'
+            'questions_marked=EXCLUDED.questions_marked,'
+            'questions_correct=EXCLUDED.questions_correct,max_marks=EXCLUDED.max_marks,'
+            'marks_obtained=EXCLUDED.marks_obtained,cheated=EXCLUDED.cheated;',
+            (
+                transform_row[0],
+                transform_row[1],
+                transform_row[2],
+                transform_row[3],
+                transform_row[4],
+                transform_row[5],
+                transform_row[6],
+                transform_row[7],
+                transform_row[8],
+                transform_row[9],
+                transform_row[10],
+                transform_row[11],
+                transform_row[12],
+                transform_row[13],
+                transform_row[14],
+                transform_row[15],
+                transform_row[16],
+                transform_row[17],
+            )
+        )
+    pg_conn.commit()
+
+
+dag = DAG(
+    'ARL_Assessments_x_user',
+    default_args=default_args,
+    description='An Analytics Reporting Layer DAG for Assessments x user level',
+    schedule_interval='35 0 * * *',
+    catchup=False
+)
+
+create_table = PostgresOperator(
+    task_id='create_table',
+    postgres_conn_id='postgres_result_db',
+    sql='''CREATE TABLE IF NOT EXISTS arl_assessments_x_user (
+            id serial,
+            table_unique_key bigint not null PRIMARY KEY,
+            user_id bigint,
+            course_id int,
+            assessment_id int,
+            assessment_title varchar(256),
+            assessment_type varchar(32),
+            assessment_sub_type varchar(16),
+            assessment_release_date DATE,
+            assessment_open_date DATE,
+            assessment_submission_date DATE,
+            assessment_attempt_status varchar(32),
+            assessment_submission_status varchar(32),
+            question_count int,
+            questions_marked int,
+            questions_correct int,
+            max_marks int,
+            marks_obtained int,
+            cheated boolean
+        );
+    ''',
+    dag=dag
+)
+
+transform_data = PostgresOperator(
+    task_id='transform_data',
+    postgres_conn_id='postgres_result_db',
+    sql='''select 
+            cast(concat(assessments.course_id, assessments.assessment_id, course_user_mapping.user_id) as bigint) as table_unique_key,
+            course_user_mapping.user_id,
+            assessments.course_id,
+            assessments.assessment_id,
+            assessments.title as assessment_title,
+            
+            case
+                when assessments.assessment_type = 1 then 'Normal Assessment'
+                when assessments.assessment_type = 2 then 'Filtering Assessment'
+                when assessments.assessment_type = 3 then 'Competitive Assessment'
+                when assessments.assessment_type = 4 then 'Duration Assessment'
+                when assessments.assessment_type = 5 then 'Poll Assessment'
+            end as assessment_type,
+            
+            case 
+                when assessments.sub_type = 1 then 'General'
+                when assessments.sub_type = 2 then 'In-Class'
+                when assessments.sub_type = 3 then 'Post-Class'
+                when assessments.sub_type = 4 then 'Classroom-Quiz'
+            end as assessment_sub_type,
+            
+            date(assessments.start_timestamp) as assessment_release_date,
+            date(assessment_question_user_mapping.assessment_started_at) as assessment_open_date,
+            date(assessment_question_user_mapping.assessment_completed_at) as assessment_submission_date,
+        
+            case 
+                when assessment_question_user_mapping.assessment_started_at <= assessments.end_timestamp then 'Attempted On Time'
+                when assessment_question_user_mapping.assessment_started_at > assessments.end_timestamp then 'Late Attempt'
+                when assessment_question_user_mapping.assessment_started_at is null then 'Not Attempted'
+            end as assessment_attempt_status,
+        
+            case 
+                when assessment_late_completed = false then 'On Time Submission'
+                when assessment_late_completed = true then 'Late Submission'
+                when assessment_late_completed is null then 'Not Submitted'
+            end as assessment_submission_status,
+            
+            assessments.question_count,
+            count(distinct mcq_id) filter (where option_marked_at is not null) as questions_marked,
+            count(distinct mcq_id) filter (where (marked_choice - correct_choice) = 0) as questions_correct,
+            assessments.max_marks,
+            assessment_question_user_mapping.marks_obtained,
+            assessment_question_user_mapping.cheated
+        from
+            assessments
+        join courses
+            on courses.course_id = assessments.course_id and course_structure_id in (1,6,8,11,12,14,18,19,20,22,23,26)
+        join course_user_mapping
+            on course_user_mapping.course_id = assessments.course_id and status in (5,8,9) and label_id is null and lower(course_user_mapping.unit_type) like 'learning'
+        left join assessment_question_user_mapping
+            on assessment_question_user_mapping.assessment_id = assessments.assessment_id and assessment_question_user_mapping.course_user_mapping_id = course_user_mapping.course_user_mapping_id
+        group by 1,2,3,4,5,6,7,8,9,10,11,12,13,16,17,18;
+        ''',
+    dag=dag
+)
+
+extract_python_data = PythonOperator(
+    task_id='extract_python_data',
+    python_callable=extract_data_to_nested,
+    provide_context=True,
+    dag=dag
+)
+create_table >> transform_data >> extract_python_data
