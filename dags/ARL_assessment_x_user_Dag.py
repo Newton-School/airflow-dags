ba4b8bf7b8ebf0b416ba212046e0d70e08e18ba4
aniket_vijay <aniket.vijay@newtonschool.co>
2023-06-29 15:49:44 +0530

    incorporating changes in assessments domain in arl users cross assessments
    Signed-off-by: aniket_vijay <aniket.vijay@newtonschool.co>

--- dags/ARL_assessment_x_user_Dag.py
+++ dags/ARL_assessment_x_user_Dag.py
@@ -1,11 +1,19 @@
 from airflow import DAG
+# from airflow.decorators import dag
 from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.models import Variable
+from airflow.utils.task_group import TaskGroup
 from datetime import datetime
+
+from sqlalchemy_utils.types.enriched_datetime.pendulum_date import pendulum
 
 default_args = {
     'owner': 'airflow',
+    'max_active_tasks': 6,
+    'max_active_runs': 6,
+    'concurrency': 4,
     'depends_on_past': False,
     'start_date': datetime(2023, 3, 16),
 }
@@ -28,22 +36,28 @@ def extract_data_to_nested(**kwargs):
     for transform_row in transform_data_output:
         pg_cursor.execute(
             'INSERT INTO arl_assessments_x_user (table_unique_key,user_id,course_id,assessment_id,'
-            'assessment_title,assessment_type,assessment_sub_type,assessment_release_date,'
+            'assessment_title,assessment_type,assessment_sub_type,generation_and_creation_type,'
+            'assessment_class,assessment_release_date,'
             'assessment_open_date,assessment_submission_date,assessment_attempt_status,'
             'assessment_submission_status,question_count,questions_marked,questions_correct,'
             'max_marks,marks_obtained,cheated)'
-            'VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
-            'on conflict (table_unique_key) do update set assessment_sub_type=Excluded.assessment_sub_type,'
-            'assessment_type=Excluded.assessment_type,'
-            'assessment_title=Excluded.assessment_title, '
-            'assessment_open_date=Excluded.assessment_open_date,'
-            'assessment_submission_date=EXCLUDED.assessment_submission_date,'
-            'assessment_attempt_status=EXCLUDED.assessment_attempt_status,'
-            'assessment_submission_status=EXCLUDED.assessment_submission_status,'
-            'question_count=EXCLUDED.question_count,'
-            'questions_marked=EXCLUDED.questions_marked,'
-            'questions_correct=EXCLUDED.questions_correct,max_marks=EXCLUDED.max_marks,'
-            'marks_obtained=EXCLUDED.marks_obtained,cheated=EXCLUDED.cheated;',
+            'VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+            'on conflict (table_unique_key) do update set assessment_title = EXCLUDED.assessment_title,'
+            'assessment_type = EXCLUDED.assessment_type,'
+            'assessment_sub_type = EXCLUDED.assessment_sub_type,'
+            'generation_and_creation_type = EXCLUDED.generation_and_creation_type,'
+            'assessment_class = EXCLUDED.assessment_class,'
+            'assessment_release_date = EXCLUDED.assessment_release_date,'
+            'assessment_open_date = EXCLUDED.assessment_open_date,'
+            'assessment_submission_date = EXCLUDED.assessment_submission_date,'
+            'assessment_attempt_status = EXCLUDED.assessment_attempt_status,'
+            'assessment_submission_status = EXCLUDED.assessment_submission_status,'
+            'question_count = EXCLUDED.question_count,'
+            'questions_marked = EXCLUDED.questions_marked,'
+            'questions_correct = EXCLUDED.questions_correct,'
+            'max_marks = EXCLUDED.max_marks,'
+            'marks_obtained = EXCLUDED.marks_obtained,'
+            'cheated = EXCLUDED.cheated;',
             (
                 transform_row[0],
                 transform_row[1],
@@ -63,6 +77,8 @@ def extract_data_to_nested(**kwargs):
                 transform_row[15],
                 transform_row[16],
                 transform_row[17],
+                transform_row[18],
+                transform_row[19]
             )
         )
     pg_conn.commit()
@@ -71,6 +87,9 @@ def extract_data_to_nested(**kwargs):
 dag = DAG(
     'ARL_Assessments_x_user',
     default_args=default_args,
+    concurrency=4,
+    max_active_tasks=6,
+    max_active_runs=6,
     description='An Analytics Reporting Layer DAG for Assessments x user level',
     schedule_interval='35 0 * * *',
     catchup=False
@@ -84,10 +103,12 @@ create_table = PostgresOperator(
             table_unique_key bigint not null PRIMARY KEY,
             user_id bigint,
             course_id int,
-            assessment_id int,
+            assessment_id bigint,
             assessment_title varchar(256),
             assessment_type varchar(32),
             assessment_sub_type varchar(16),
+            generation_and_creation_type varchar(256),
+            assessment_class varchar(256),
             assessment_release_date DATE,
             assessment_open_date DATE,
             assessment_submission_date DATE,
@@ -128,6 +149,24 @@ transform_data = PostgresOperator(
                 when assessments.sub_type = 3 then 'Post-Class'
                 when assessments.sub_type = 4 then 'Classroom-Quiz'
             end as assessment_sub_type,
+            
+            case 
+            	when assessments.generation_and_creation_type = 1 then 'Fully Automated'
+            	when assessments.generation_and_creation_type = 2 then 'Manually Released'
+            	when assessments.generation_and_creation_type = 3 then 'Manually Created'
+            	when assessments.generation_and_creation_type = 4 then 'Fully Manual'
+            end as generation_and_creation_type,
+                
+            case 
+            	when (assessments.assessment_type = 1 and assessments.sub_type = 1 and assessments.generation_and_creation_type = 1) then 'General Quiz'
+                when (assessments.assessment_type = 1 and assessments.sub_type = 2 and assessments.generation_and_creation_type = 1) then 'In-Class Automated Quiz'
+            	when (assessments.assessment_type = 1 and assessments.sub_type = 3 and assessments.generation_and_creation_type = 1) then 'Post-Class Automated Quiz'
+            	when assessments.assessment_type = 1 and assessments.generation_and_creation_type = 4 then 'Instructor Quiz'
+            	when assessments.assessment_type = 1 and assessments.generation_and_creation_type = 2 then 'Classroom Quiz'
+            	when assessments.assessment_type = 2 then 'Filtering Assessment'
+            	when assessments.assessment_type in (3,4) then 'MCQ-Contest'
+            	when assessments.assessment_type = 5 then 'Poll Assessment'
+            end as assessment_class,
 
             date(assessments.start_timestamp) as assessment_release_date,
             date(assessment_question_user_mapping.assessment_started_at) as assessment_open_date,
@@ -159,7 +198,7 @@ transform_data = PostgresOperator(
             on course_user_mapping.course_id = assessments.course_id and status in (5,8,9) and label_id is null and lower(course_user_mapping.unit_type) like 'learning'
         left join assessment_question_user_mapping
             on assessment_question_user_mapping.assessment_id = assessments.assessment_id and assessment_question_user_mapping.course_user_mapping_id = course_user_mapping.course_user_mapping_id
-        group by 1,2,3,4,5,6,7,8,9,10,11,12,13,16,17,18;
+        group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,18,19,20;
         ''',
     dag=dag
 )
