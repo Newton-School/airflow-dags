265520a8d1e5e5239c70c0d209e92b02e05fc148
aniket_vijay <aniket.vijay@newtonschool.co>
2023-07-24 12:32:58 +0530

    reported question table re-push
    Signed-off-by: aniket_vijay <aniket.vijay@newtonschool.co>

--- dags/ARL_assignment_question_report_dag.py
+++ dags/ARL_assignment_question_report_dag.py
@@ -90,8 +90,7 @@ def extract_data_to_nested(**kwargs):
                 transform_row[15],
                 transform_row[16],
                 transform_row[17],
-                transform_row[18]
-
+                transform_row[18],
             )
         )
     pg_conn.commit()
@@ -113,7 +112,7 @@ create_table = PostgresOperator(
     postgres_conn_id='postgres_result_db',
     sql='''CREATE TABLE IF NOT EXISTS arl_assignment_reported_question (
             id serial,
-            table_unique_key varchar(1028) not null PRIMARY KEY,
+            table_unique_key text not null PRIMARY KEY,
             user_id bigint,
             course_id int,
             status int,
@@ -123,9 +122,9 @@ create_table = PostgresOperator(
             question_report_date date,
             question_report_date_week timestamp,
             assignment_question_id bigint,
-            question_title varchar(2056),
+            question_title text,
             feedback_question_id int,
-            question_text varchar(2048),
+            question_text text,
             inaccurate_difficulty varchar(16),
             question_description_not_clear varchar(16),
             input_unclear_or_incorrect varchar(16),
@@ -141,91 +140,94 @@ transform_data = PostgresOperator(
     task_id='transform_data',
     postgres_conn_id='postgres_result_db',
     sql='''with feedback_raw as
-	(select 
-		user_id,
-		date(ffar.completed_at) as question_report_date,
-		aq.assignment_question_id,
-		aq.question_title,
-		ffaq.feedback_question_id,
-		ffaq.question_text,
-		case when ffar.feedback_answer_id = 190 then 'Yes' else null end inaccurate_difficulty,
-		case when ffar.feedback_answer_id = 189 then 'Yes' else null end question_description_not_clear,
-		case when ffar.feedback_answer_id = 193 then 'Yes' else null end as input_unclear_or_incorrect,
-		case when ffar.feedback_answer_id = 191 then 'Yes' else null end as required_topics_not_taught,
-		case when ffar.feedback_answer_id = 194 then 'Yes' else null end as expected_output_is_inaccurate,
-		case when ffar.feedback_answer_id = 192 then 'Yes' else null end as test_cases_missing_or_wrong
-	from 
-		feedback_form_all_responses ffar 
-	join feedback_forms_and_questions ffaq 
-		on ffaq.feedback_form_id = ffar.feedback_form_id
-			and ffar.feedback_form_id = 4419 and ffar.feedback_answer_id in (189,190,191,192,193,194)
-	join assignment_question aq
-		on aq.assignment_question_id = ffar.entity_object_id and entity_content_type_id = 62
-	where ffaq.feedback_question_id = 335
-	order by completed_at desc),
+        (select 
+            user_id,
+            date(ffar.completed_at) as question_report_date,
+            aq.assignment_question_id,
+            aq.question_title,
+            ffaq.feedback_question_id,
+            ffaq.question_text,
+            case when ffar.feedback_answer_id = 190 then 'Yes' else null end inaccurate_difficulty,
+            case when ffar.feedback_answer_id = 189 then 'Yes' else null end question_description_not_clear,
+            case when ffar.feedback_answer_id = 193 then 'Yes' else null end as input_unclear_or_incorrect,
+            case when ffar.feedback_answer_id = 191 then 'Yes' else null end as required_topics_not_taught,
+            case when ffar.feedback_answer_id = 194 then 'Yes' else null end as expected_output_is_inaccurate,
+            case when ffar.feedback_answer_id = 192 then 'Yes' else null end as test_cases_missing_or_wrong
+        from 
+            feedback_form_all_responses ffar 
+        join feedback_forms_and_questions ffaq 
+            on ffaq.feedback_form_id = ffar.feedback_form_id
+                and ffar.feedback_form_id = 4419 and ffar.feedback_answer_id in (189,190,191,192,193,194)
+        join assignment_question aq
+            on aq.assignment_question_id = ffar.entity_object_id 
+                and entity_content_type_id = 62
+        where ffaq.feedback_question_id = 335
+        order by completed_at desc),
 
 
-question_release_date as 
-	(with question_data as 
-		(select 
-			'Normal Assignment' as assignment_flow_type,
-			assignment_id,
-			course_id,
-			question_id,
-			null as user_id
-		from
-			assignment_question_mapping aqm 
-		union 
-		select 
-			'Random Assignment' as assignment_flow_type,
-			assignment_id,
-			course_id,
-			question_id,
-			user_id
-		from
-			assignment_random_question_mapping arqm)
-		
-		
-	select 
-		question_data.*,
-		date(a.start_timestamp) as assignment_release_date
-	from
-		question_data
-	join assignments a 
-		on a.assignment_id = question_data.assignment_id
-	order by 2 desc, 1)
-
-select
-	concat(extract('month' from question_report_date), extract('day' from question_report_date), assignments.assignment_id, feedback_raw.user_id,'1' ,assignment_question_id, course_user_mapping.course_id) as table_unique_key,
-	feedback_raw.user_id,
-	course_user_mapping.course_id,
-	course_user_mapping.status,
-	assignments.assignment_id,
-	date(assignments.start_timestamp) as assignment_release_date,
-	date_trunc('week', assignments.start_timestamp) as assignment_release_date_week,
-	question_report_date,
-	date_trunc('week', question_report_date) as question_report_date_week,
-	assignment_question_id,
-	question_title,
-	feedback_question_id,
-	question_text,
-	max(inaccurate_difficulty) as inaccurate_difficulty,
-	max(question_description_not_clear) as question_description_not_clear,
-	max(input_unclear_or_incorrect) as input_unclear_or_incorrect,
-	max(required_topics_not_taught) as required_topics_not_taught,
-	max(expected_output_is_inaccurate) as expected_output_is_inaccurate,
-	max(test_cases_missing_or_wrong) as test_cases_missing_or_wrong
-from
-	feedback_raw
-left join course_user_mapping
-	on course_user_mapping.user_id = feedback_raw.user_id
-left join assignments
-	on assignments.course_id = course_user_mapping.course_id
-left join question_release_date
-	on assignments.assignment_id = question_release_date.assignment_id 
-		and feedback_raw.assignment_question_id = question_release_date.question_id 
-			and course_user_mapping.course_id = question_release_date.course_id
-group by 1,2,3,4,5,6,7,8,9,10,11,12,13;
+        question_release_date as 
+            (with question_data as 
+                (select 
+                    'Normal Assignment' as assignment_flow_type,
+                    assignment_id,
+                    course_id,
+                    question_id,
+                    null as user_id
+                from
+                    assignment_question_mapping aqm 
+                union 
+                select 
+                    'Random Assignment' as assignment_flow_type,
+                    assignment_id,
+                    course_id,
+                    question_id,
+                    user_id
+                from
+                    assignment_random_question_mapping arqm)
+                
+                
+            select 
+                question_data.*,
+                date(a.start_timestamp) as assignment_release_date
+            from
+                question_data
+            join assignments a 
+                on a.assignment_id = question_data.assignment_id
+                    and a.hidden = false
+            order by 2 desc, 1)
+        
+        select
+            concat(extract('month' from question_report_date), extract('day' from question_report_date), assignments.assignment_id, feedback_raw.user_id,'1' ,assignment_question_id, course_user_mapping.course_id) as table_unique_key,
+            feedback_raw.user_id,
+            course_user_mapping.course_id,
+            course_user_mapping.status,
+            assignments.assignment_id,
+            date(assignments.start_timestamp) as assignment_release_date,
+            date_trunc('week', assignments.start_timestamp) as assignment_release_date_week,
+            question_report_date,
+            date_trunc('week', question_report_date) as question_report_date_week,
+            assignment_question_id,
+            question_title,
+            feedback_question_id,
+            question_text,
+            max(inaccurate_difficulty) as inaccurate_difficulty,
+            max(question_description_not_clear) as question_description_not_clear,
+            max(input_unclear_or_incorrect) as input_unclear_or_incorrect,
+            max(required_topics_not_taught) as required_topics_not_taught,
+            max(expected_output_is_inaccurate) as expected_output_is_inaccurate,
+            max(test_cases_missing_or_wrong) as test_cases_missing_or_wrong
+        from
+            feedback_raw
+        left join course_user_mapping
+            on course_user_mapping.user_id = feedback_raw.user_id
+        left join assignments
+            on assignments.course_id = course_user_mapping.course_id
+                and assignments.hidden = false 
+        left join question_release_date
+            on assignments.assignment_id = question_release_date.assignment_id 
+                and feedback_raw.assignment_question_id = question_release_date.question_id 
+                    and course_user_mapping.course_id = question_release_date.course_id
+        group by 1,2,3,4,5,6,7,8,9,10,11,12,13;
         ''',
     dag=dag
 )
