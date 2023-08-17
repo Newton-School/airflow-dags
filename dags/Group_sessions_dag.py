12c0542006b89586a63f20d9cadd84194d2f6468
aniket_vijay <aniket.vijay@newtonschool.co>
2023-08-17 12:29:41 +0530

    changing ADL group sessions table
    Signed-off-by: aniket_vijay <aniket.vijay@newtonschool.co>

--- dags/Group_sessions_dag.py
+++ dags/Group_sessions_dag.py
@@ -27,30 +27,34 @@ def extract_data_to_nested(**kwargs):
     transform_data_output = ti.xcom_pull(task_ids='transform_data')
     for transform_row in transform_data_output:
         pg_cursor.execute(
-            'INSERT INTO group_sessions (meeting_id, booked_by_id, mentee_user_id, child_video_session,'
-            'course_id, actual_duration, created_at, start_timestamp, end_timestamp, end_via_api,hash,'
-            'participants_count, reports_pulled, title, video_session_using, with_mentees, is_deleted,'
+            'INSERT INTO group_sessions (table_unique_key, meeting_id, '
+            'booked_by_id, mentee_user_id, child_video_session,'
+            'course_id, actual_duration, created_at, start_timestamp, end_timestamp, '
+            'end_via_api,hash, participants_count, reports_pulled, '
+            'title, video_session_using, with_mentees, is_deleted,'
             'deleted_by_id, should_redirect, cancel_reason, meeting_status)'
-            'VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
-            'on conflict (meeting_id) do update set booked_by_id=EXCLUDED.booked_by_id,'
+            'VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+            'on conflict (table_unique_key) do update set meeting_id = EXCLUDED.meeting_id,'
+            'booked_by_id = EXCLUDED.booked_by_id,'
             'mentee_user_id = EXCLUDED.mentee_user_id,'
-            'child_video_session=EXCLUDED.child_video_session,'
-            'course_id=EXCLUDED.course_id,'
-            'actual_duration=EXCLUDED.actual_duration,'
-            'start_timestamp=EXCLUDED.start_timestamp,'
-            'end_timestamp=EXCLUDED.end_timestamp,'
-            'end_via_api=EXCLUDED.end_via_api,'
-            'hash=EXCLUDED.hash,'
-            'participants_count=EXCLUDED.participants_count,'
-            'reports_pulled=EXCLUDED.reports_pulled,'
-            'title=EXCLUDED.title,'
-            'video_session_using=EXCLUDED.video_session_using,'
-            'with_mentees=EXCLUDED.with_mentees,'
-            'is_deleted=EXCLUDED.is_deleted,'
-            'deleted_by_id=EXCLUDED.deleted_by_id,'
-            'should_redirect=EXCLUDED.should_redirect,'
-            'cancel_reason=EXCLUDED.cancel_reason,'
-            'meeting_status=EXCLUDED.meeting_status ;',
+            'child_video_session = EXCLUDED.child_video_session,'
+            'course_id = EXCLUDED.course_id,'
+            'actual_duration = EXCLUDED.actual_duration,'
+            'created_at = EXCLUDED.created_at,'
+            'start_timestamp = EXCLUDED.start_timestamp,'
+            'end_timestamp = EXCLUDED.end_timestamp,'
+            'end_via_api = EXCLUDED.end_via_api,'
+            'hash = EXCLUDED.hash,'
+            'participants_count = EXCLUDED.participants_count,'
+            'reports_pulled = EXCLUDED.reports_pulled,'
+            'title = EXCLUDED.title,'
+            'video_session_using = EXCLUDED.video_session_using,'
+            'with_mentees = EXCLUDED.with_mentees,'
+            'is_deleted = EXCLUDED.is_deleted,'
+            'deleted_by_id = EXCLUDED.deleted_by_id,'
+            'should_redirect = EXCLUDED.should_redirect,'
+            'cancel_reason = EXCLUDED.cancel_reason,'
+            'meeting_status = EXCLUDED.meeting_status ;',
             (
                 transform_row[0],
                 transform_row[1],
@@ -73,6 +77,7 @@ def extract_data_to_nested(**kwargs):
                 transform_row[18],
                 transform_row[19],
                 transform_row[20],
+                transform_row[21],
             )
         )
     pg_conn.commit()
@@ -92,7 +97,8 @@ create_table = PostgresOperator(
     postgres_conn_id='postgres_result_db',
     sql='''CREATE TABLE IF NOT EXISTS group_sessions (
             id serial,
-            meeting_id int not null PRIMARY KEY,
+            table_unique_key not null text,
+            meeting_id int,
             booked_by_id bigint,
             mentee_user_id bigint,
             child_video_session boolean,
@@ -122,6 +128,7 @@ transform_data = PostgresOperator(
     task_id='transform_data',
     postgres_conn_id='postgres_read_replica',
     sql='''select
+                concat(video_sessions_meeting.id, video_sessions_meeting_booked_with.user_id) as table_unique_key,
                 video_sessions_meeting.id as meeting_id,
                 booked_by_id,
                 video_sessions_meeting_booked_with.user_id as mentee_user_id,
