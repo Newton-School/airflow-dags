dffc297ff5200c8e783f42636daa2ca8f55e4abe
aniket_vijay <aniket.vijay@newtonschool.co>
2023-08-28 12:41:13 +0530

    wow active batches changes, btech addition
    Signed-off-by: aniket_vijay <aniket.vijay@newtonschool.co>

--- dags/WoW_Active_batches_dag.py
+++ dags/WoW_Active_batches_dag.py
@@ -88,7 +88,7 @@ from
             *,
             case
                 when (lu_csid in (1,18) and ((lu_start_date <= current_date) and (au_start_date + interval '15 Days') <= current_date) and au_end_date>= current_date and lu_end_date >= current_date) then 'Active'
-                when (lu_csid in (6,8,11,12,13,14,19,20,22,23,26) and (au_start_date <= current_date) and au_end_date>= current_date and lu_end_date >= current_date) then 'Active'
+                when (lu_csid in (6,8,7,11,12,13,14,19,20,22,23,26,44) and (au_start_date <= current_date) and au_end_date>= current_date and lu_end_date >= current_date) then 'Active'
                 else 'Inactive' end as batch_active_status_excluding_i2_check,
             case 
                 when lu_course_id in (
@@ -149,6 +149,7 @@ from
             when lu_csid in (1,18) then 'PAP'
             when lu_csid in (6,8,12,13,19,22,23) then 'Upfront - Non Data Science'
             when lu_csid in (11,14,20,26) then 'Data Science + IU'
+            when lu_csid in (7,44) then 'Newton School of Technology'
             else 'No Tag' 
         end as course_type,
         au_course_id,
@@ -206,7 +207,7 @@ left join
                             on courses_course.id = courses_courseusermapping.course_id
                         left join auth_user
                             on auth_user.id = courses_courseusermapping.user_id
-                        where ((courses_course.course_structure_id in (6,8,11,12,13,14,19,20,22,23,26,24,32) and courses_courseusermapping.status in (8)) or (courses_course.course_structure_id in (1,18) and courses_courseusermapping.status in (5,9)))
+                        where ((courses_course.course_structure_id in (6,8,7,11,12,13,14,19,20,22,23,26,24,32,44) and courses_courseusermapping.status in (8)) or (courses_course.course_structure_id in (1,18) and courses_courseusermapping.status in (5,9)))
                         )
                         
                         
