d9d894293a79322c4f8971711fee1942e1c880dc
aniket_vijay <aniket.vijay@newtonschool.co>
2023-08-31 15:18:11 +0530

    adding btech program data in arl tables
    Signed-off-by: aniket_vijay <aniket.vijay@newtonschool.co>

--- dags/ARL_assessment_x_user_Dag.py
+++ dags/ARL_assessment_x_user_Dag.py
@@ -249,7 +249,7 @@ transform_data = PostgresOperator(
         from
             assessments
         join courses
-            on courses.course_id = assessments.course_id and course_structure_id in (1,6,8,11,12,14,18,19,20,22,23,26,34) and date(assessments.start_timestamp) >= '2022-07-01'
+            on courses.course_id = assessments.course_id and course_structure_id in (1,6,7,8,11,12,14,18,19,20,22,23,26,34,44) and date(assessments.start_timestamp) >= '2022-07-01'
         		and courses.course_id in (select distinct wab.lu_course_id from wow_active_batches wab)
         join course_user_mapping
             on course_user_mapping.course_id = courses.course_id and status in (8,9,11,12,30)
