43b59bfe5adb410fe2596c4dddcc4dae37222b80
harshalmahajan <harshal.mahajan@newtonschool.co>
2023-09-14 14:13:18 +0530

    Fix
    Signed-off-by: harshalmahajan <harshal.mahajan@newtonschool.co>

--- dags/LSQ_Leads_data.py
+++ dags/LSQ_Leads_data.py
@@ -350,7 +350,7 @@ transform_data = PostgresOperator(
                 
             FROM leadsquareleadsdata l2
             left join leadsquareactivity l on l2.prospectid = l.relatedprospectid 
-            where date(l2.createdon) >= 'February 1,2023' and date(l2.createdon) < 'June 1,2023'
+            where date(l2.createdon) >= 'June 1,2023'
             order by 2,5;
         ''',
     dag=dag
