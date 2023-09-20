27038804290b1e18afb261e0fa437786b2eeb5c1
aniket_vijay <aniket.vijay@newtonschool.co>
2023-09-20 20:00:39 +0530

    adding subjective responses for reported questions
    Signed-off-by: aniket_vijay <aniket.vijay@newtonschool.co>

--- dags/ARL_assignment_question_report_dag.py
+++ dags/ARL_assignment_question_report_dag.py
@@ -145,7 +145,7 @@ transform_data = PostgresOperator(
     task_id='transform_data',
     postgres_conn_id='postgres_result_db',
     sql='''with feedback_raw as
-        (select 
+        (select
             user_id,
             date(ffar.completed_at) as question_report_date,
             aq.assignment_question_id,
@@ -157,16 +157,17 @@ transform_data = PostgresOperator(
             case when ffar.feedback_answer_id = 193 then 'Yes' else null end as input_unclear_or_incorrect,
             case when ffar.feedback_answer_id = 191 then 'Yes' else null end as required_topics_not_taught,
             case when ffar.feedback_answer_id = 194 then 'Yes' else null end as expected_output_is_inaccurate,
-            case when ffar.feedback_answer_id = 192 then 'Yes' else null end as test_cases_missing_or_wrong
+            case when ffar.feedback_answer_id = 192 then 'Yes' else null end as test_cases_missing_or_wrong,
+            case when ffar.feedback_answer_id is null then ffar.feedback_answer end as subjective_answer
         from 
             feedback_form_all_responses_new ffar 
         join feedback_forms_and_questions ffaq 
             on ffaq.feedback_form_id = ffar.feedback_form_id
-                and ffar.feedback_form_id = 4419 and ffar.feedback_answer_id in (189,190,191,192,193,194)
+                and ffar.feedback_form_id = 4419
         join assignment_question aq
             on aq.assignment_question_id = ffar.entity_object_id 
                 and entity_content_type_id = 62
-        where ffaq.feedback_question_id = 335
+        where ffaq.feedback_question_id in (335, 336)
         order by completed_at desc),
 
 
@@ -229,8 +230,8 @@ transform_data = PostgresOperator(
             	when course_user_mapping.status in (25) then 'Mock Interviewer'
             	when course_user_mapping.status is null then 'Free User'
             	else 'Other' 
-            end as user_type
-            
+            end as user_type,
+            subjective_answer
         from
             feedback_raw
         left join course_user_mapping
@@ -248,7 +249,7 @@ transform_data = PostgresOperator(
 		    on aq.topic_id  = t.topic_id 
 		    	and t.topic_template_id in (102,103,119,334,336,338,339,340,341,342,344,410)
 
-        group by 1,2,3,4,5,6,7,8,9,10,11,12,19,20,21;
+        group by 1,2,3,4,5,6,7,8,9,10,11,12,19,20,21,22;
         ''',
     dag=dag
 )
