9b83d921e46472079c02e32e90c0f1ea57a9cbab
aniket_vijay <aniket.vijay@newtonschool.co>
2023-09-29 23:00:23 +0530

    adding topics table fields
    Signed-off-by: aniket_vijay <aniket.vijay@newtonschool.co>

--- dags/course_component_mapping_DAG.py
+++ dags/course_component_mapping_DAG.py
@@ -17,26 +17,29 @@ def extract_data_to_nested(**kwargs):
     transform_data_output = ti.xcom_pull(task_ids='transform_data')
     for transform_row in transform_data_output:
         pg_cursor.execute(
-            'INSERT INTO course_component_mapping (table_unique_key,'
+            'INSERT INTO course_component_topic_module_mapping (table_unique_key,'
             'course_id,'
             'course_name,'
             'course_component_id,'
             'module_name,'
             'topic_template_id,'
-            'topic_name,'
-            'topic_deadline_timestamp,'
-            'topic_unlock_timestamp,'
-            'topic_clearance_points,'
+            'module_topic_name,'
+            'topic_topic_id,'
+            'topic_topic_name,'
+            'module_topic_deadline_timestamp,'
+            'module_topic_unlock_timestamp,'
+            'module_topic_clearance_points,'
             'module_deadline_timestamp,'
             'module_unlock_timestamp,'
             'module_clearance_points) '
-            'VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+            'VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
             'on conflict (table_unique_key) do update set course_name = EXCLUDED.course_name,'
             'module_name = EXCLUDED.module_name,'
-            'topic_name = EXCLUDED.topic_name,'
-            'topic_deadline_timestamp = EXCLUDED.topic_deadline_timestamp,'
-            'topic_unlock_timestamp = EXCLUDED.topic_unlock_timestamp,'
-            'topic_clearance_points = EXCLUDED.topic_clearance_points,'
+            'module_topic_name = EXCLUDED.module_topic_name,'
+            'topic_topic_name = EXCLUDED.topic_topic_name,'
+            'module_topic_deadline_timestamp = EXCLUDED.module_topic_deadline_timestamp,'
+            'module_topic_unlock_timestamp = EXCLUDED.module_topic_unlock_timestamp,'
+            'module_topic_clearance_points = EXCLUDED.module_topic_clearance_points,'
             'module_deadline_timestamp = EXCLUDED.module_deadline_timestamp,'
             'module_unlock_timestamp = EXCLUDED.module_unlock_timestamp,'
             'module_clearance_points = EXCLUDED.module_clearance_points;',
@@ -54,15 +57,17 @@ def extract_data_to_nested(**kwargs):
                 transform_row[10],
                 transform_row[11],
                 transform_row[12],
+                transform_row[13],
+                transform_row[14],
             )
         )
     pg_conn.commit()
 
 
 dag = DAG(
-    'course_component_mapping_dag',
+    'course_component_topic_module_mapping_dag',
     default_args=default_args,
-    description='Course component mapping at module and topic (module accdng to normal nomenclature) level',
+    description='Course component mapping at module and topic (module accdng to normal nomenclature) and with technologies_topics.id level',
     schedule_interval='30 20 * * *',
     catchup=False
 )
@@ -70,7 +75,7 @@ dag = DAG(
 create_table = PostgresOperator(
     task_id='create_table',
     postgres_conn_id='postgres_result_db',
-    sql='''CREATE TABLE IF NOT EXISTS course_component_mapping (
+    sql='''CREATE TABLE IF NOT EXISTS course_component_topic_module_mapping (
             id serial,
             table_unique_key text not null PRIMARY KEY,
             course_id int, 
@@ -78,10 +83,12 @@ create_table = PostgresOperator(
             course_component_id int,
             module_name text,
             topic_template_id int,
-            topic_name text,
-            topic_deadline_timestamp timestamp, 
-            topic_unlock_timestamp timestamp,
-            topic_clearance_points int,
+            module_topic_name text,
+            topic_topic_id int,
+            topic_topic_name text,
+            module_topic_deadline_timestamp timestamp, 
+            module_topic_unlock_timestamp timestamp,
+            module_topic_clearance_points int,
             module_deadline_timestamp timestamp,
             module_unlock_timestamp timestamp,
             module_clearance_points int
@@ -95,16 +102,18 @@ transform_data = PostgresOperator(
     postgres_conn_id='postgres_read_replica',
     sql='''
         select 
-            concat(courses_course.id,'_',cc2.object_id) as table_unique_key,
+            concat(courses_course.id,'_',cc2.object_id,'_',technologies_topic.id) as table_unique_key,
             courses_course.id as course_id,
             courses_course.title as course_name,
             courses_coursecomponent.id as course_component_id,
             courses_coursecomponent.title as module_name,
             cc2.object_id as topic_template_id,
-            cc2.title as topic_name,
-            cc2.deadline_timestamp as topic_deadline_timestamp,
-            cc2.unlock_timestamp as topic_unlock_timestamp,
-            cc2.clearance_points as topic_clearance_points,
+            cc2.title as module_topic_name,
+            technologies_topic.id as topic_topic_id,
+            technologies_topic.title as topic_topic_name,
+            cc2.deadline_timestamp as module_topic_deadline_timestamp,
+            cc2.unlock_timestamp as module_topic_unlock_timestamp,
+            cc2.clearance_points as module_topic_clearance_points,
             courses_coursecomponent.deadline_timestamp as module_deadline_timestamp,
             courses_coursecomponent.unlock_timestamp as module_unlock_timestamp,
             courses_coursecomponent.clearance_points as module_clearance_points
@@ -114,6 +123,10 @@ transform_data = PostgresOperator(
             on courses_course.id = courses_coursecomponent.course_id
         join courses_coursecomponent cc2
             on cc2.parent_id = courses_coursecomponent.id
+        join technologies_topicnode
+            on technologies_topicnode.topic_template_id = cc2.object_id
+        join technologies_topic
+            on technologies_topic.id = technologies_topicnode.topic_id
         where courses_coursecomponent.parent_id is null
         and courses_coursecomponent.component_type = 2
         and courses_coursecomponent.unlock_timestamp <= current_date
