81b9d144c9af585f38ed8c607f648846ea4c358d
aniket_vijay <aniket.vijay@newtonschool.co>
2023-09-29 21:51:40 +0530

    adding course component mapping table
    Signed-off-by: aniket_vijay <aniket.vijay@newtonschool.co>

---
+++ dags/course_component_mapping_DAG.py
@@ -0,0 +1,130 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from datetime import datetime
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 3, 16),
+}
+def extract_data_to_nested(**kwargs):
+    pg_hook = PostgresHook(postgres_conn_id='postgres_result_db')
+    pg_conn = pg_hook.get_conn()
+    pg_cursor = pg_conn.cursor()
+    ti = kwargs['ti']
+    transform_data_output = ti.xcom_pull(task_ids='transform_data')
+    for transform_row in transform_data_output:
+        pg_cursor.execute(
+            'INSERT INTO course_component_mapping (table_unique_key,'
+            'course_id,'
+            'course_name,'
+            'course_component_id,'
+            'module_name,'
+            'topic_template_id,'
+            'topic_name,'
+            'topic_deadline_timestamp,'
+            'topic_unlock_timestamp,'
+            'topic_clearance_points,'
+            'module_deadline_timestamp,'
+            'module_unlock_timestamp,'
+            'module_clearance_points) '
+            'VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+            'on conflict (table_unique_key) do update set course_name = EXCLUDED.course_name,'
+            'module_name =EXCLUDED.module_name,'
+            'topic_name = EXCLUDED.topic_name,'
+            'topic_deadline_timestamp = EXCLUDED.topic_deadline_timestamp,'
+            'topic_unlock_timestamp = EXCLUDED.topic_unlock_timestamp,'
+            'topic_clearance_points = EXCLUDED.topic_clearance_points,'
+            'module_deadline_timestamp = EXCLUDED.module_deadline_timestamp,'
+            'module_unlock_timestamp = EXCLUDED.module_unlock_timestamp,'
+            'module_clearance_points = EXCLUDED.module_clearance_points;',
+            (
+                transform_row[0],
+                transform_row[1],
+                transform_row[2],
+                transform_row[3],
+                transform_row[4],
+                transform_row[5],
+                transform_row[6],
+                transform_row[7],
+                transform_row[8],
+                transform_row[9],
+                transform_row[10],
+                transform_row[11],
+                transform_row[12],
+            )
+        )
+    pg_conn.commit()
+
+
+dag = DAG(
+    'course_component_mapping_dag',
+    default_args=default_args,
+    description='Course component mapping at module and topic (module accdng to normal nomenclature) level',
+    schedule_interval='30 20 * * *',
+    catchup=False
+)
+
+create_table = PostgresOperator(
+    task_id='create_table',
+    postgres_conn_id='postgres_result_db',
+    sql='''CREATE TABLE IF NOT EXISTS course_component_mapping (
+            id serial,
+            table_unique_key text not null PRIMARY KEY,
+            course_id int, 
+            course_component_id int,
+            module_name text,
+            topic_template_id int,
+            topic_name text,
+            topic_deadline_timestamp, 
+            topic_unlock_timestamp,
+            topic_clearance_points int,
+            module_deadline_timestamp,
+            module_unlock_timestamp,
+            module_clearance_points int
+        );
+    ''',
+    dag=dag
+)
+
+transform_data = PostgresOperator(
+    task_id='transform_data',
+    postgres_conn_id='postgres_read_replica',
+    sql='''
+        select 
+            concat(courses_course.id,'_',cc2.object_id) as table_unique_key,
+            courses_course.id as course_id,
+            courses_course.title as course_name,
+            courses_coursecomponent.id as course_component_id,
+            courses_coursecomponent.title as module_name,
+            cc2.object_id as topic_template_id,
+            cc2.title as topic_name,
+            cc2.deadline_timestamp as topic_deadline_timestamp,
+            cc2.unlock_timestamp as topic_unlock_timestamp,
+            cc2.clearance_points as topic_clearance_points,
+            courses_coursecomponent.deadline_timestamp as module_deadline_timestamp,
+            courses_coursecomponent.unlock_timestamp as module_unlock_timestamp,
+            courses_coursecomponent.clearance_points as module_clearance_points
+        from
+            courses_coursecomponent
+        join courses_course
+            on courses_course.id = courses_coursecomponent.course_id
+        join courses_coursecomponent cc2
+            on cc2.parent_id = courses_coursecomponent.id
+        where courses_coursecomponent.parent_id is null
+        and courses_coursecomponent.component_type = 2
+        and courses_coursecomponent.unlock_timestamp <= current_date
+        order by 3;
+        ''',
+    dag=dag
+)
+
+extract_python_data = PythonOperator(
+    task_id='extract_python_data',
+    python_callable=extract_data_to_nested,
+    provide_context=True,
+    dag=dag
+)
+create_table >> transform_data >> extract_python_data
