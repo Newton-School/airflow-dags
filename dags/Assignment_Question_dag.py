5f3eec4cfbd1033620de6f23331244da9e9d69a5
aniket_vijay <aniket.vijay@newtonschool.co>
2023-10-11 15:26:05 +0530

    test cases field update
    Signed-off-by: aniket_vijay <aniket.vijay@newtonschool.co>

--- dags/Assignment_Question_dag.py
+++ dags/Assignment_Question_dag.py
@@ -27,8 +27,38 @@ def extract_data_to_nested(**kwargs):
     transform_data_output = ti.xcom_pull(task_ids='transform_data')
     for transform_row in transform_data_output:
         pg_cursor.execute(
-            'INSERT INTO assignment_question (assignment_question_id,created_at,created_by_id,hash,is_deleted,max_points,max_marks,peer_reviewed,peer_reviewed_by_id,question_for_assignment_type,question_title,question_type,test_case_count,verified,feedback_evaluable,rating,difficulty_type,mandatory,topic_id,question_utility_type,relevance) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
-            'on conflict (assignment_question_id) do update set peer_reviewed = EXCLUDED.peer_reviewed, peer_reviewed_by_id = EXCLUDED.peer_reviewed_by_id, question_for_assignment_type = EXCLUDED.question_for_assignment_type, question_title = EXCLUDED.question_title, test_case_count = EXCLUDED.test_case_count, verified = EXCLUDED.verified, rating = EXCLUDED.rating, difficulty_type = EXCLUDED.difficulty_type, mandatory = EXCLUDED.mandatory, topic_id = EXCLUDED.topic_id, question_utility_type = EXCLUDED.question_utility_type, relevance = EXCLUDED.relevance;',
+            'INSERT INTO assignment_question (assignment_question_id,'
+            'created_at,'
+            'created_by_id,'
+            'hash,'
+            'is_deleted,'
+            'max_points,'
+            'max_marks,'
+            'peer_reviewed,'
+            'peer_reviewed_by_id,'
+            'question_for_assignment_type,'
+            'question_title,'
+            'question_type,'
+            'test_case_count,'
+            'verified,'
+            'feedback_evaluable,'
+            'rating,'
+            'difficulty_type,'
+            'mandatory,'
+            'topic_id,'
+            'question_utility_type,'
+            'relevance) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+            'on conflict (assignment_question_id) do update set peer_reviewed = EXCLUDED.peer_reviewed, '
+            'peer_reviewed_by_id = EXCLUDED.peer_reviewed_by_id, '
+            'question_for_assignment_type = EXCLUDED.question_for_assignment_type, '
+            'question_title = EXCLUDED.question_title, '
+            'test_case_count = EXCLUDED.test_case_count, '
+            'verified = EXCLUDED.verified, '
+            'rating = EXCLUDED.rating, difficulty_type = EXCLUDED.difficulty_type, '
+            'mandatory = EXCLUDED.mandatory, '
+            'topic_id = EXCLUDED.topic_id, '
+            'question_utility_type = EXCLUDED.question_utility_type, '
+            'relevance = EXCLUDED.relevance;',
             (
                 transform_row[0],
                 transform_row[1],
@@ -68,17 +98,18 @@ create_table = PostgresOperator(
     task_id='create_table',
     postgres_conn_id='postgres_result_db',
     sql='''CREATE TABLE IF NOT EXISTS assignment_question (
+            id serial,
             assignment_question_id bigint not null PRIMARY KEY,
             created_at timestamp,
             created_by_id bigint,
-            hash varchar(128),
+            hash text,
             is_deleted boolean,
             max_points int,
             max_marks int,
             peer_reviewed boolean,
             peer_reviewed_by_id bigint,
             question_for_assignment_type integer[],
-            question_title varchar(256),
+            question_title text,
             question_type int,
             test_case_count int,
             verified boolean,
@@ -109,8 +140,8 @@ transform_data = PostgresOperator(
     assignments_assignmentquestion.peer_reviewed_by_id,
     assignments_assignmentquestion.question_for_assignment_type,
     assignments_assignmentquestion.question_title,
-    assignments_assignmentquestion.question_type, 
-    assignments_assignmentquestion.test_cases_count,
+    assignments_assignmentquestion.question_type,
+    total_test_cases as test_cases_count,
     assignments_assignmentquestion.verified,
     assignments_assignmentquestion.feedback_evaluable,
     assignments_assignmentquestion.rating, 
@@ -122,7 +153,14 @@ transform_data = PostgresOperator(
 from
     assignments_assignmentquestion
 left join assignments_assignmentquestiontopicmapping
-    on assignments_assignmentquestiontopicmapping.assignment_question_id = assignments_assignmentquestion.id and main_topic = true;
+    on assignments_assignmentquestiontopicmapping.assignment_question_id = assignments_assignmentquestion.id and main_topic = true
+left join 
+    (select
+            assignment_question_id,
+            count(distinct id) as total_test_cases
+        from
+            assignments_assignmentquestiontestcasemapping
+        group by 1) test_cases_count on test_cases_count.assignment_question_id = assignments_assignmentquestion.id;
         ''',
     dag=dag
 )
