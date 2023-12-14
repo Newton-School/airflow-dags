ae085cb01c1fc41f019f09921d0f471296c61b20
harshalmahajan <harshal.mahajan@newtonschool.co>
2023-12-14 19:19:42 +0530

    Add Masterclass DAG
    Signed-off-by: harshalmahajan <harshal.mahajan@newtonschool.co>

---
+++ dags/master_class_dashboard.py
@@ -0,0 +1,122 @@
+from airflow import DAG
+# from airflow.decorators import dag
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.models import Variable
+from airflow.utils.task_group import TaskGroup
+from datetime import datetime
+
+from sqlalchemy_utils.types.enriched_datetime.pendulum_date import pendulum
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 3, 16),
+}
+
+
+def extract_data_to_nested(**kwargs):
+    def clean_input(data_type, data_value):
+        if data_type == 'string':
+            return 'null' if not data_value else f'\"{data_value}\"'
+        elif data_type == 'datetime':
+            return 'null' if not data_value else f'CAST(\'{data_value}\' As TIMESTAMP)'
+        else:
+            return data_value
+
+    pg_hook = PostgresHook(postgres_conn_id='postgres_result_db')
+    pg_conn = pg_hook.get_conn()
+    pg_cursor = pg_conn.cursor()
+    ti = kwargs['ti']
+    transform_data_output = ti.xcom_pull(task_ids='transform_data')
+    for transform_row in transform_data_output:
+        pg_cursor.execute(
+            'INSERT INTO master_class_dashboard ()'
+            'VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);',
+            (
+                transform_row[0],
+                transform_row[1],
+                transform_row[2],
+                transform_row[3],
+                transform_row[4],
+                transform_row[5],
+                transform_row[6],
+                transform_row[7],
+                transform_row[8],
+                transform_row[9],
+                transform_row[10],
+                transform_row[11],
+                transform_row[12],
+                transform_row[13],
+                transform_row[14],
+                transform_row[15],
+                transform_row[16],
+                transform_row[17],
+                transform_row[18],
+                transform_row[19],
+                transform_row[20],
+                transform_row[21],
+                transform_row[22],
+                transform_row[23],
+                transform_row[24],
+                transform_row[25],
+                transform_row[26],
+                transform_row[27],
+                transform_row[28],
+                transform_row[29],
+                transform_row[30],
+                transform_row[31],
+                transform_row[32],
+                transform_row[33],
+                transform_row[34],
+                transform_row[35],
+                transform_row[36],
+                transform_row[37],
+            )
+        )
+    pg_conn.commit()
+
+
+dag = DAG(
+    'Master_Class_Dashboard_DAG',
+    default_args=default_args,
+    description='An Analytics Reporting Layer DAG for Master Class Dashboard',
+    schedule_interval='30 2 * * *',
+    catchup=False
+)
+
+create_table = PostgresOperator(
+    task_id='create_table',
+    postgres_conn_id='postgres_result_db',
+    sql='''CREATE TABLE IF NOT EXISTS master_class_dashboard (
+            id serial,
+            
+        );
+    ''',
+    dag=dag
+)
+
+transform_data = PostgresOperator(
+    task_id='transform_data',
+    postgres_conn_id='postgres_result_db',
+    sql='''
+    ;
+        ''',
+    dag=dag
+)
+drop_table = PostgresOperator(
+    task_id='drop_table',
+    postgres_conn_id='postgres_result_db',
+    sql='''DROP TABLE IF EXISTS master_class_dashboard;
+    ''',
+    dag=dag
+)
+
+extract_python_data = PythonOperator(
+    task_id='extract_python_data',
+    python_callable=extract_data_to_nested,
+    provide_context=True,
+    dag=dag
+)
+drop_table >> create_table >> transform_data >> extract_python_data
