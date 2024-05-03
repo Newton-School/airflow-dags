8702079c72ed60c8a7057a7c97b15450951991d6
Drumil Patel <drumilpatel720@gmail.com>
2024-05-03 16:11:09 +0530

    Fox
    Signed-off-by: Drumil Patel <drumilpatel720@gmail.com>

--- dags/job_posting_limit_offset_DAG.py
+++ dags/job_posting_limit_offset_DAG.py
@@ -144,7 +144,7 @@ def get_postgres_job_posting_operator(task_iterator):
                 job_openings._airbyte_job_openings_hashid,
                 job_openings._airbyte_unique_key,
                 raw_response -> 'vacancy' as number_of_openings
-                from job_openings limit {{{{ ti.xcom_pull(key="query_limit_job_posting_{task_iterator}", task_ids="transform_limit_offset_{task_iterator}") }}}} offset {{{{ ti.xcom_pull(key="query_offset_job_posting_{task_iterator}", task_ids="transform_limit_offset_{task_iterator}") }}}};
+                from job_openings limit {{{{ ti.xcom_pull("query_limit_job_posting_{task_iterator}") }}}} offset {{{{ ti.xcom_pull("query_offset_job_posting_{task_iterator}") }}}};
             ''',
         dag=dag
     )
