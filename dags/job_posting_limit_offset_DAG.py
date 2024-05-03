0d72237366e45c232018204103f5407467600c8b
Drumil Patel <drumilpatel720@gmail.com>
2024-05-03 15:15:44 +0530

    Fix Airflow Dags
    Signed-off-by: Drumil Patel <drumilpatel720@gmail.com>

--- dags/job_posting_limit_offset_DAG.py
+++ dags/job_posting_limit_offset_DAG.py
@@ -173,8 +173,8 @@ for job_postings_sub_dag_id in range(int(total_number_of_sub_dags)):
             query_limit = math.floor(individual_total_job_posting)
             query_offset = individual_total_job_posting * kwargs["current_iterator"]
 
-            print("Drumil", query_limit, query_offset)
-            
+            ti.xcom_push(key=f"query_limit_job_posting_{job_postings_sub_dag_id}", value=query_limit)
+            ti.xcom_push(key=f"query_offset_job_posting_{job_postings_sub_dag_id}", value=query_offset)
 
 
         transform_limit_offset = PythonOperator(
