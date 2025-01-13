b49a8fc9de2fa128f47b908781fe785f80048c84
Gaurav Kapatia <gkapatia@gmail.com>
2025-01-13 10:18:57 +0530

    updated placements_company_dag

--- dags/placements_company_dag.py
+++ dags/placements_company_dag.py
@@ -26,31 +26,55 @@ def extract_data_to_nested(**kwargs):
     transform_data_output = ti.xcom_pull(task_ids='transform_data')
     for transform_row in transform_data_output:
         pg_cursor.execute(
-                'INSERT INTO placements_company (company_id,company_name,company_type,created_at,created_by_id,'
-                'information,remark,slug,status,updated_at,topic_id,key_account_manager_1,'
-                'key_account_manager_2,sales_poc_1,sales_poc_2) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
-                'on conflict (company_id) do update set updated_at =EXCLUDED.updated_at,topic_id =EXCLUDED.topic_id,'
-                'key_account_manager_1 =EXCLUDED.key_account_manager_1,'
-                'key_account_manager_2 =EXCLUDED.key_account_manager_2,sales_poc_1 =EXCLUDED.sales_poc_1,'
-                'sales_poc_2 =EXCLUDED.sales_poc_2,information =EXCLUDED.information,remark =EXCLUDED.remark,'
-                'status =EXCLUDED.status,company_name =EXCLUDED.company_name,company_type =EXCLUDED.company_type ;',
-                (
-                    transform_row[0],
-                    transform_row[1],
-                    transform_row[2],
-                    transform_row[3],
-                    transform_row[4],
-                    transform_row[5],
-                    transform_row[6],
-                    transform_row[7],
-                    transform_row[8],
-                    transform_row[9],
-                    transform_row[10],
-                    transform_row[11],
-                    transform_row[12],
-                    transform_row[13],
-                    transform_row[14],
-                 )
+            '''
+            INSERT INTO placements_company (
+                company_id,
+                company_name,
+                company_type,
+                created_at,
+                created_by_id,
+                information,
+                remark,
+                slug,
+                status,
+                updated_at,
+                topic_id,
+                key_account_manager_1,
+                key_account_manager_2,
+                sales_poc_1,
+                sales_poc_2
+            ) 
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            ON CONFLICT (company_id) DO UPDATE SET
+                updated_at = EXCLUDED.updated_at,
+                topic_id = EXCLUDED.topic_id,
+                key_account_manager_1 = EXCLUDED.key_account_manager_1,
+                key_account_manager_2 = EXCLUDED.key_account_manager_2,
+                sales_poc_1 = EXCLUDED.sales_poc_1,
+                sales_poc_2 = EXCLUDED.sales_poc_2,
+                information = EXCLUDED.information,
+                remark = EXCLUDED.remark,
+                status = EXCLUDED.status,
+                company_name = EXCLUDED.company_name,
+                company_type = EXCLUDED.company_type;
+            ''',
+            (
+                transform_row[0],  # company_id
+                transform_row[1],  # company_name
+                transform_row[2],  # company_type
+                transform_row[3],  # created_at
+                transform_row[4],  # created_by_id
+                transform_row[5],  # information
+                transform_row[6],  # remark
+                transform_row[7],  # slug
+                transform_row[8],  # status
+                transform_row[9],  # updated_at
+                transform_row[10], # topic_id
+                transform_row[11], # key_account_manager_1
+                transform_row[12], # key_account_manager_2
+                transform_row[13], # sales_poc_1
+                transform_row[14]  # sales_poc_2
+            )
         )
     pg_conn.commit()
 
@@ -67,22 +91,56 @@ create_table = PostgresOperator(
     task_id='create_table',
     postgres_conn_id='postgres_result_db',
     sql='''CREATE TABLE IF NOT EXISTS placements_company (
-            company_id bigint not null PRIMARY KEY,
-            company_name varchar(256),
-            company_type varchar(10),
-            created_at TIMESTAMP,
-            created_by_id bigint,
-            information varchar(4500),
-            remark varchar(3000),
-            slug varchar(128),
-            status int,
-            updated_at TIMESTAMP,
-            topic_id bigint,
-            key_account_manager_1 varchar(128),
-            key_account_manager_2 varchar(128),
-            sales_poc_1 varchar(128),
-            sales_poc_2 varchar(128)
-        );
+                company_id BIGINT NOT NULL PRIMARY KEY,
+                company_name VARCHAR(256),
+                company_type VARCHAR(10),
+                created_at TIMESTAMP,
+                created_by_id BIGINT,
+                information VARCHAR(4500),
+                remark VARCHAR(3000),
+                slug VARCHAR(128),
+                status INT,
+                updated_at TIMESTAMP,
+                topic_id BIGINT,
+                key_account_manager_1 VARCHAR(128),
+                key_account_manager_2 VARCHAR(128),
+                sales_poc_1 VARCHAR(128),
+                sales_poc_2 VARCHAR(128)
+            );
+    ''',
+    dag=dag
+)
+
+alter_table_v1 = PostgresOperator(
+    task_id='alter_table',
+    postgres_conn_id='postgres_result_db',
+    sql='''
+        DO $$
+        BEGIN
+            -- Check if the 'information' column is not of type TEXT
+            IF EXISTS (
+                SELECT 1
+                FROM information_schema.columns
+                WHERE table_name = 'placements_company'
+                  AND column_name = 'information'
+                  AND data_type != 'text'
+            ) THEN
+                -- Alter 'information' column to TEXT
+                ALTER TABLE placements_company ALTER COLUMN information TYPE TEXT;
+            END IF;
+
+            -- Check if the 'remark' column is not of type TEXT
+            IF EXISTS (
+                SELECT 1
+                FROM information_schema.columns
+                WHERE table_name = 'placements_company'
+                  AND column_name = 'remark'
+                  AND data_type != 'text'
+            ) THEN
+                -- Alter 'remark' column to TEXT
+                ALTER TABLE placements_company ALTER COLUMN remark TYPE TEXT;
+            END IF;
+        END $$;
     ''',
     dag=dag
 )
@@ -90,57 +148,87 @@ create_table = PostgresOperator(
 transform_data = PostgresOperator(
     task_id='transform_data',
     postgres_conn_id='postgres_read_replica',
-    sql='''with enterprise as(
-            select distinct company_id
-            from placements_company
-            left join placements_companylabelmapping on placements_companylabelmapping.company_id = placements_company.id
-            left join technologies_label on technologies_label.id = placements_companylabelmapping.label_id
-            where placements_companylabelmapping.label_id = 395 and technologies_label.name = 'Enterprise'
+    sql='''WITH enterprise AS (
+                SELECT DISTINCT 
+                    company_id
+                FROM 
+                    placements_company
+                LEFT JOIN 
+                    placements_companylabelmapping ON placements_companylabelmapping.company_id = placements_company.id
+                LEFT JOIN 
+                    technologies_label ON technologies_label.id = placements_companylabelmapping.label_id
+                WHERE 
+                    placements_companylabelmapping.label_id = 395 
+                    AND technologies_label.name = 'Enterprise'
             ),
-            kam as(
-            select 
-            distinct company_id, concat(first_name,' ',last_name) as key_account_manager,
-            rank() over (partition by company_id order by concat(first_name,' ',last_name)) as rn
-            from placements_companyplacementcoordinatormapping
-            left join trainers_placementcoordinator on trainers_placementcoordinator.id = placements_companyplacementcoordinatormapping.placement_coordinator_id
-            left join auth_user on trainers_placementcoordinator.user_id = auth_user.id
-            where placements_companyplacementcoordinatormapping.company_placement_coordinator_mapping_type = 2
-            order by 1
+
+            kam AS (
+                SELECT DISTINCT 
+                    company_id, 
+                    CONCAT(first_name, ' ', last_name) AS key_account_manager,
+                    RANK() OVER (PARTITION BY company_id ORDER BY CONCAT(first_name, ' ', last_name)) AS rn
+                FROM 
+                    placements_companyplacementcoordinatormapping
+                LEFT JOIN 
+                    trainers_placementcoordinator ON trainers_placementcoordinator.id = placements_companyplacementcoordinatormapping.placement_coordinator_id
+                LEFT JOIN 
+                    auth_user ON trainers_placementcoordinator.user_id = auth_user.id
+                WHERE 
+                    placements_companyplacementcoordinatormapping.company_placement_coordinator_mapping_type = 2
+                ORDER BY 
+                    company_id
             ),
-            sales as(
-            select 
-            distinct company_id, concat(first_name,' ',last_name) as sales_poc,
-            rank() over (partition by company_id order by concat(first_name,' ',last_name)) as rn
-            from placements_companyplacementcoordinatormapping
-            left join trainers_placementcoordinator on trainers_placementcoordinator.id = placements_companyplacementcoordinatormapping.placement_coordinator_id
-            left join auth_user on trainers_placementcoordinator.user_id = auth_user.id
-            where placements_companyplacementcoordinatormapping.company_placement_coordinator_mapping_type = 1
-            order by 1
+
+            sales AS (
+                SELECT DISTINCT 
+                    company_id, 
+                    CONCAT(first_name, ' ', last_name) AS sales_poc,
+                    RANK() OVER (PARTITION BY company_id ORDER BY CONCAT(first_name, ' ', last_name)) AS rn
+                FROM 
+                    placements_companyplacementcoordinatormapping
+                LEFT JOIN 
+                    trainers_placementcoordinator ON trainers_placementcoordinator.id = placements_companyplacementcoordinatormapping.placement_coordinator_id
+                LEFT JOIN 
+                    auth_user ON trainers_placementcoordinator.user_id = auth_user.id
+                WHERE 
+                    placements_companyplacementcoordinatormapping.company_placement_coordinator_mapping_type = 1
+                ORDER BY 
+                    company_id
             )
-            select
-            distinct placements_company.id as company_id,
-            placements_company.title as company_name,
-            case when enterprise.company_id is not null then 'Enterprise'
-            else 'Startup' end as company_type,
-            placements_company.created_at,
-            placements_company.created_by_id,
-            placements_company.description as information,
-            placements_company.remark,
-            placements_company.slug,
-            placements_company.status,
-            placements_company.updated_at,
-            placements_company.topic_id,
-            kam1.key_account_manager as key_account_manager_1,
-            kam2.key_account_manager as key_account_manager_2,
-            sales1.sales_poc as sales_poc_1,
-            sales2.sales_poc as sales_poc_2
-            from placements_company
-            left join enterprise on enterprise.company_id = placements_company.id
-            left join kam as kam1 on kam1.company_id = placements_company.id and kam1.rn = 1
-            left join kam as kam2 on kam2.company_id = placements_company.id and kam2.rn = 2
-            left join sales as sales1 on sales1.company_id = placements_company.id and sales1.rn = 1
-            left join sales as sales2 on sales2.company_id = placements_company.id and sales2.rn = 2
-            order by 1;
+
+            SELECT DISTINCT 
+                placements_company.id AS company_id,
+                placements_company.title AS company_name,
+                CASE 
+                    WHEN enterprise.company_id IS NOT NULL THEN 'Enterprise'
+                    ELSE 'Startup' 
+                END AS company_type,
+                placements_company.created_at,
+                placements_company.created_by_id,
+                placements_company.description AS information,
+                placements_company.remark,
+                placements_company.slug,
+                placements_company.status,
+                placements_company.updated_at,
+                placements_company.topic_id,
+                kam1.key_account_manager AS key_account_manager_1,
+                kam2.key_account_manager AS key_account_manager_2,
+                sales1.sales_poc AS sales_poc_1,
+                sales2.sales_poc AS sales_poc_2
+            FROM 
+                placements_company
+            LEFT JOIN 
+                enterprise ON enterprise.company_id = placements_company.id
+            LEFT JOIN 
+                kam AS kam1 ON kam1.company_id = placements_company.id AND kam1.rn = 1
+            LEFT JOIN 
+                kam AS kam2 ON kam2.company_id = placements_company.id AND kam2.rn = 2
+            LEFT JOIN 
+                sales AS sales1 ON sales1.company_id = placements_company.id AND sales1.rn = 1
+            LEFT JOIN 
+                sales AS sales2 ON sales2.company_id = placements_company.id AND sales2.rn = 2
+            ORDER BY 
+                company_id;
         ''',
     dag=dag
 )
@@ -151,4 +239,4 @@ extract_python_data = PythonOperator(
     provide_context=True,
     dag=dag
 )
-create_table >> transform_data >> extract_python_data
+create_table >> alter_table_v1 >> transform_data >> extract_python_data
