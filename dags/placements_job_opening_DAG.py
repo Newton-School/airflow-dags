5f9a8cd530cfc63982f2e666d157c6d54e6a4c8d
Gaurav Kapatia <gkapatia@gmail.com>
2025-01-13 10:26:08 +0530

    updated placements_job_opening_DAG

--- dags/placements_job_opening_DAG.py
+++ dags/placements_job_opening_DAG.py
@@ -86,102 +86,175 @@ dag = DAG(
     catchup=False
 )
 
+# Create a table for placements job openings
 create_table = PostgresOperator(
     task_id='create_table',
     postgres_conn_id='postgres_result_db',
-    sql='''CREATE TABLE IF NOT EXISTS placements_job_openings (
-            job_opening_id bigint not null PRIMARY KEY,
-            company_id bigint,
-            city_id int,
+    sql='''
+        CREATE TABLE IF NOT EXISTS placements_job_openings (
+            job_opening_id BIGINT NOT NULL PRIMARY KEY,
+            company_id BIGINT,
+            city_id INT,
             created_at TIMESTAMP,
-            created_by_id bigint,
-            job_description varchar(6500),
-            hash varchar(64),
-            interview_process varchar(3500),
-            max_ctc int,
-            min_ctc int,
-            number_of_openings int,
-            short_description varchar(300),
-            job_title varchar(128),
+            created_by_id BIGINT,
+            job_description VARCHAR(6500),
+            hash VARCHAR(64),
+            interview_process VARCHAR(3500),
+            max_ctc INT,
+            min_ctc INT,
+            number_of_openings INT,
+            short_description VARCHAR(300),
+            job_title VARCHAR(128),
             updated_at TIMESTAMP,
-            number_of_rounds int,
+            number_of_rounds INT,
             closure_date DATE,
-            placement_role_title varchar(32),
-            employment_type int,
-            allow_auto_referral boolean,
-            company_criteria_under_grad text[],
-            company_criteria_post_grad text[],
-            company_criteria_under_grad_field_of_study text[],
-            company_criteria_post_grad_field_of_study text[],
-            company_criteria_under_grad_percentage text[],
-            company_criteria_post_grad_percentage text[],
-            company_criteria_tenth_percent text[],
-            company_criteria_twelfth_percent text[],
-            company_criteria_gender text[],
-            company_criteria_overall_work_experience_match_type int,
-            company_criteria_overall_work_experience text[],
-            company_criteria_notice_period text[],
-            company_criteria_bond text[],
-            company_criteria_technical_work_experience text[]
+            placement_role_title VARCHAR(32),
+            employment_type INT,
+            allow_auto_referral BOOLEAN,
+            
+            -- Company criteria fields
+            company_criteria_under_grad TEXT[],
+            company_criteria_post_grad TEXT[],
+            company_criteria_under_grad_field_of_study TEXT[],
+            company_criteria_post_grad_field_of_study TEXT[],
+            company_criteria_under_grad_percentage TEXT[],
+            company_criteria_post_grad_percentage TEXT[],
+            company_criteria_tenth_percent TEXT[],
+            company_criteria_twelfth_percent TEXT[],
+            company_criteria_gender TEXT[],
+            company_criteria_overall_work_experience_match_type INT,
+            company_criteria_overall_work_experience TEXT[],
+            company_criteria_notice_period TEXT[],
+            company_criteria_bond TEXT[],
+            company_criteria_technical_work_experience TEXT[]
         );
     ''',
     dag=dag
 )
 
+alter_table_v1 = PostgresOperator(
+    task_id='alter_table',
+    postgres_conn_id='postgres_result_db',
+    sql='''
+        DO $$
+        BEGIN
+            -- Check if the column already has a length of 255
+            IF NOT EXISTS (
+                SELECT 1 
+                FROM information_schema.columns 
+                WHERE table_name = 'placements_job_openings' 
+                  AND column_name = 'placement_role_title'
+                  AND character_maximum_length = 255
+            ) THEN
+                -- Alter the column to set character length to 255
+                ALTER TABLE placements_job_openings 
+                ALTER COLUMN placement_role_title TYPE VARCHAR(255);
+            END IF;
+        END $$;
+    ''',
+    dag=dag
+)
+
+# Task to transform data from the database
 transform_data = PostgresOperator(
     task_id='transform_data',
     postgres_conn_id='postgres_read_replica',
-    sql='''select
-            placements_jobopening.id as job_opening_id,
+    sql='''
+        SELECT
+            placements_jobopening.id AS job_opening_id,
             placements_jobopening.company_id,
             placements_jobopening.city_id,
             placements_jobopening.created_at,
             placements_jobopening.created_by_id,
-            placements_jobopening.description as job_description,
+            placements_jobopening.description AS job_description,
             placements_jobopening.hash,
             placements_jobopening.interview_process,
             placements_jobopening.max_ctc,
             placements_jobopening.min_ctc,
             placements_jobopening.number_of_openings,
             placements_jobopening.short_description,
-            placements_jobopening.title as job_title,
+            placements_jobopening.title AS job_title,
             placements_jobopening.updated_at,
             placements_jobopening.number_of_rounds,
             placements_jobopening.closure_date,
-            placements_placementrole.title as placement_role_title,
+            placements_placementrole.title AS placement_role_title,
             placements_jobopening.employment_type,
             placements_jobopening.allow_auto_referral,
-            placements_jobopeningcriteriaug.match_values as company_criteria_under_grad,
-            placements_jobopeningcriteriapg.match_values as company_criteria_post_grad,
-            placements_jobopeningcriteriaugfs.match_values as company_criteria_under_grad_field_of_study,
-            placements_jobopeningcriteriapgfs.match_values as company_criteria_post_grad_field_of_study,
-            placements_jobopeningcriteriaugpercent.match_values as company_criteria_under_grad_percentage,
-            placements_jobopeningcriteriapgpercent.match_values as company_criteria_post_grad_percentage,
-            placements_jobopeningcriteriatenthpercent.match_values as company_criteria_tenth_percent,
-            placements_jobopeningcriteriatwelfthpercent.match_values as company_criteria_twelfth_percent,
-            placements_jobopeningcriteriagender.match_values as company_criteria_gender,
-            placements_jobopeningcriteriaowe.match_type as company_criteria_overall_work_experience_match_type,
-            placements_jobopeningcriteriaowe.match_values as company_criteria_overall_work_experience,
-            placements_jobopeningcriterianoticeperiod.match_values as company_criteria_notice_period,
-            placements_jobopeningcriteriabond.match_values as company_criteria_bond,
-            placements_jobopeningcriteriatechnicalworkex.match_values as company_criteria_technical_work_experience
             
-            from placements_jobopening
-            left join placements_placementrole on placements_placementrole.id = placements_jobopening.placement_role_id
-            left join placements_jobopeningcriteria as placements_jobopeningcriteriaug on placements_jobopeningcriteriaug.job_opening_id = placements_jobopening.id and placements_jobopeningcriteriaug.criteria_type = 1
-            left join placements_jobopeningcriteria as placements_jobopeningcriteriapg on placements_jobopeningcriteriapg.job_opening_id = placements_jobopening.id and placements_jobopeningcriteriapg.criteria_type = 2
-            left join placements_jobopeningcriteria as placements_jobopeningcriteriaugfs on placements_jobopeningcriteriaugfs.job_opening_id = placements_jobopening.id and placements_jobopeningcriteriaugfs.criteria_type = 3
-            left join placements_jobopeningcriteria as placements_jobopeningcriteriapgfs on placements_jobopeningcriteriapgfs.job_opening_id = placements_jobopening.id and placements_jobopeningcriteriapgfs.criteria_type = 4
-            left join placements_jobopeningcriteria as placements_jobopeningcriteriaugpercent on placements_jobopeningcriteriaugpercent.job_opening_id = placements_jobopening.id and placements_jobopeningcriteriaugpercent.criteria_type = 5
-            left join placements_jobopeningcriteria as placements_jobopeningcriteriapgpercent on placements_jobopeningcriteriapgpercent.job_opening_id = placements_jobopening.id and placements_jobopeningcriteriapgpercent.criteria_type = 6
-            left join placements_jobopeningcriteria as placements_jobopeningcriteriatenthpercent on placements_jobopeningcriteriatenthpercent.job_opening_id = placements_jobopening.id and placements_jobopeningcriteriatenthpercent.criteria_type = 7
-            left join placements_jobopeningcriteria as placements_jobopeningcriteriatwelfthpercent on placements_jobopeningcriteriatwelfthpercent.job_opening_id = placements_jobopening.id and placements_jobopeningcriteriatwelfthpercent.criteria_type = 8
-            left join placements_jobopeningcriteria as placements_jobopeningcriteriagender on placements_jobopeningcriteriagender.job_opening_id = placements_jobopening.id and placements_jobopeningcriteriagender.criteria_type = 9
-            left join placements_jobopeningcriteria as placements_jobopeningcriteriaowe on placements_jobopeningcriteriaowe.job_opening_id = placements_jobopening.id and placements_jobopeningcriteriaowe.criteria_type = 12
-            left join placements_jobopeningcriteria as placements_jobopeningcriterianoticeperiod on placements_jobopeningcriterianoticeperiod.job_opening_id = placements_jobopening.id and placements_jobopeningcriterianoticeperiod.criteria_type = 13
-            left join placements_jobopeningcriteria as placements_jobopeningcriteriabond on placements_jobopeningcriteriabond.job_opening_id = placements_jobopening.id and placements_jobopeningcriteriabond.criteria_type = 14
-            left join placements_jobopeningcriteria as placements_jobopeningcriteriatechnicalworkex on placements_jobopeningcriteriatechnicalworkex.job_opening_id = placements_jobopening.id and placements_jobopeningcriteriatechnicalworkex.criteria_type = 16;
-        ''',
+            -- Company criteria fields
+            placements_jobopeningcriteriaug.match_values AS company_criteria_under_grad,
+            placements_jobopeningcriteriapg.match_values AS company_criteria_post_grad,
+            placements_jobopeningcriteriaugfs.match_values AS company_criteria_under_grad_field_of_study,
+            placements_jobopeningcriteriapgfs.match_values AS company_criteria_post_grad_field_of_study,
+            placements_jobopeningcriteriaugpercent.match_values AS company_criteria_under_grad_percentage,
+            placements_jobopeningcriteriapgpercent.match_values AS company_criteria_post_grad_percentage,
+            placements_jobopeningcriteriatenthpercent.match_values AS company_criteria_tenth_percent,
+            placements_jobopeningcriteriatwelfthpercent.match_values AS company_criteria_twelfth_percent,
+            placements_jobopeningcriteriagender.match_values AS company_criteria_gender,
+            placements_jobopeningcriteriaowe.match_type AS company_criteria_overall_work_experience_match_type,
+            placements_jobopeningcriteriaowe.match_values AS company_criteria_overall_work_experience,
+            placements_jobopeningcriterianoticeperiod.match_values AS company_criteria_notice_period,
+            placements_jobopeningcriteriabond.match_values AS company_criteria_bond,
+            placements_jobopeningcriteriatechnicalworkex.match_values AS company_criteria_technical_work_experience
+        
+        FROM 
+            placements_jobopening
+        LEFT JOIN 
+            placements_placementrole 
+            ON placements_placementrole.id = placements_jobopening.placement_role_id
+        LEFT JOIN 
+            placements_jobopeningcriteria AS placements_jobopeningcriteriaug 
+            ON placements_jobopeningcriteriaug.job_opening_id = placements_jobopening.id 
+            AND placements_jobopeningcriteriaug.criteria_type = 1
+        LEFT JOIN 
+            placements_jobopeningcriteria AS placements_jobopeningcriteriapg 
+            ON placements_jobopeningcriteriapg.job_opening_id = placements_jobopening.id 
+            AND placements_jobopeningcriteriapg.criteria_type = 2
+        LEFT JOIN 
+            placements_jobopeningcriteria AS placements_jobopeningcriteriaugfs 
+            ON placements_jobopeningcriteriaugfs.job_opening_id = placements_jobopening.id 
+            AND placements_jobopeningcriteriaugfs.criteria_type = 3
+        LEFT JOIN 
+            placements_jobopeningcriteria AS placements_jobopeningcriteriapgfs 
+            ON placements_jobopeningcriteriapgfs.job_opening_id = placements_jobopening.id 
+            AND placements_jobopeningcriteriapgfs.criteria_type = 4
+        LEFT JOIN 
+            placements_jobopeningcriteria AS placements_jobopeningcriteriaugpercent 
+            ON placements_jobopeningcriteriaugpercent.job_opening_id = placements_jobopening.id 
+            AND placements_jobopeningcriteriaugpercent.criteria_type = 5
+        LEFT JOIN 
+            placements_jobopeningcriteria AS placements_jobopeningcriteriapgpercent 
+            ON placements_jobopeningcriteriapgpercent.job_opening_id = placements_jobopening.id 
+            AND placements_jobopeningcriteriapgpercent.criteria_type = 6
+        LEFT JOIN 
+            placements_jobopeningcriteria AS placements_jobopeningcriteriatenthpercent 
+            ON placements_jobopeningcriteriatenthpercent.job_opening_id = placements_jobopening.id 
+            AND placements_jobopeningcriteriatenthpercent.criteria_type = 7
+        LEFT JOIN 
+            placements_jobopeningcriteria AS placements_jobopeningcriteriatwelfthpercent 
+            ON placements_jobopeningcriteriatwelfthpercent.job_opening_id = placements_jobopening.id 
+            AND placements_jobopeningcriteriatwelfthpercent.criteria_type = 8
+        LEFT JOIN 
+            placements_jobopeningcriteria AS placements_jobopeningcriteriagender 
+            ON placements_jobopeningcriteriagender.job_opening_id = placements_jobopening.id 
+            AND placements_jobopeningcriteriagender.criteria_type = 9
+        LEFT JOIN 
+            placements_jobopeningcriteria AS placements_jobopeningcriteriaowe 
+            ON placements_jobopeningcriteriaowe.job_opening_id = placements_jobopening.id 
+            AND placements_jobopeningcriteriaowe.criteria_type = 12
+        LEFT JOIN 
+            placements_jobopeningcriteria AS placements_jobopeningcriterianoticeperiod 
+            ON placements_jobopeningcriterianoticeperiod.job_opening_id = placements_jobopening.id 
+            AND placements_jobopeningcriterianoticeperiod.criteria_type = 13
+        LEFT JOIN 
+            placements_jobopeningcriteria AS placements_jobopeningcriteriabond 
+            ON placements_jobopeningcriteriabond.job_opening_id = placements_jobopening.id 
+            AND placements_jobopeningcriteriabond.criteria_type = 14
+        LEFT JOIN 
+            placements_jobopeningcriteria AS placements_jobopeningcriteriatechnicalworkex 
+            ON placements_jobopeningcriteriatechnicalworkex.job_opening_id = placements_jobopening.id 
+            AND placements_jobopeningcriteriatechnicalworkex.criteria_type = 16;
+    ''',
     dag=dag
 )
 
@@ -191,4 +264,4 @@ extract_python_data = PythonOperator(
     provide_context=True,
     dag=dag
 )
-create_table >> transform_data >> extract_python_data
+create_table >> alter_table_v1 >> transform_data >> extract_python_data
