50226240212b149b3df929b2c89d15b3ca1e1b54
Ashrithakakarla <67938143+Ashrithakakarla@users.noreply.github.com>
2025-03-26 10:31:58 +0530

    Backfilling the data from May 1 2024 to Oct 1 2024

--- dags/LSQ_Leads_data_v2.py
+++ dags/LSQ_Leads_data_v2.py
@@ -548,8 +548,8 @@ transform_data = PostgresOperator(
                 FROM leadsquareactivity 
             ) sub
             WHERE 
-                TO_TIMESTAMP(sub.createdon_ist, 'YYYY-MM-DD HH24:MI:SS') >= current_date - interval '1' year
-                
+                TO_TIMESTAMP(sub.createdon_ist, 'YYYY-MM-DD HH24:MI:SS') >= '2024-05-01 00:00:00'
+                AND TO_TIMESTAMP(sub.createdon_ist, 'YYYY-MM-DD HH24:MI:SS') < '2024-10-01 00:00:00'
             ) as l
             left join (
                 select * from (
