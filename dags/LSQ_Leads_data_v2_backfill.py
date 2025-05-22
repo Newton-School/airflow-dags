57aa2b3133570bdb0741b75ad75103a6ad6e0d8c
Mrinal Kumar <kmrinal@newtonschool.co>
2025-05-22 18:47:30 +0530

    Update backfill time interval
    Signed-off-by: Mrinal Kumar <kmrinal@newtonschool.co>

--- dags/LSQ_Leads_data_v2_backfill.py
+++ dags/LSQ_Leads_data_v2_backfill.py
@@ -534,7 +534,7 @@ transform_data = PostgresOperator(
             FROM leadsquareactivity 
         ) sub
         WHERE 
-            TO_TIMESTAMP(sub.createdon_ist, 'YYYY-MM-DD HH24:MI:SS') >= current_date - interval '365' Days
+            TO_TIMESTAMP(sub.createdon_ist, 'YYYY-MM-DD HH24:MI:SS') >= current_date - interval '365 days'
         ),
 
         lead_assigned_owners AS (
