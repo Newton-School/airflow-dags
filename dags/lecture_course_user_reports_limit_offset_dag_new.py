cb016007e3c096688d16c6a2ddbbb5797b98798b
Ashrithakakarla <67938143+Ashrithakakarla@users.noreply.github.com>
2025-06-13 15:03:57 +0530

    Backfilling for Feb-march - lcur
    Signed-off-by: Ashrithakakarla <67938143+Ashrithakakarla@users.noreply.github.com>

--- dags/lecture_course_user_reports_limit_offset_dag_new.py
+++ dags/lecture_course_user_reports_limit_offset_dag_new.py
@@ -412,8 +412,8 @@ def number_of_rows_per_lecture_sub_dag_func(start_lecture_id, end_lecture_id):
         join lectures l
             on l.course_id = c.course_id 
             and l.lecture_id BETWEEN %s AND %s
-            and (l.lecture_id in (select lecture_id from recorded_lectures_course_user_reports where lecture_watch_date BETWEEN '2025-01-01' AND '2025-01-31')
-                 or l.start_timestamp  BETWEEN '2025-01-01' AND '2025-01-31')
+            and (l.lecture_id in (select lecture_id from recorded_lectures_course_user_reports where lecture_watch_date BETWEEN '2025-01-31' AND '2025-03-31')
+                 or l.start_timestamp  BETWEEN '2025-01-31' AND '2025-03-31')
         left join user_overlapping_time let
             on let.lecture_id = l.lecture_id and let.course_user_mapping_id = cum.course_user_mapping_id
         left join recorded_lectures_course_user_reports rlcur
@@ -639,8 +639,8 @@ def transform_data_per_query(start_lecture_id, end_lecture_id, cps_sub_dag_id, c
             join lectures l
                 on l.course_id = c.course_id 
                 and l.lecture_id BETWEEN %s AND %s
-                and (l.lecture_id in (select lecture_id from recorded_lectures_course_user_reports where lecture_watch_date BETWEEN '2025-01-01' AND '2025-01-31')
-                     or l.start_timestamp  BETWEEN '2025-01-01' AND '2025-01-31')
+                and (l.lecture_id in (select lecture_id from recorded_lectures_course_user_reports where lecture_watch_date BETWEEN '2025-01-31' AND '2025-03-31')
+                     or l.start_timestamp  BETWEEN '2025-01-31' AND '2025-03-31')
             left join user_overlapping_time let
                 on let.lecture_id = l.lecture_id and let.course_user_mapping_id = cum.course_user_mapping_id
             left join recorded_lectures_course_user_reports rlcur
